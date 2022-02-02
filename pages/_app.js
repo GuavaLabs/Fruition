15ae153e7a9e25f5f98297087033e510436fb5d8
Antonio Ponce <zponce@posteo.net>
2022-02-02 18:05:50 -0500

    Added Skeleton

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
 import '../styles/globals.css'
+// <Component {...pageProps}
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return(
+    <div>
+      <header className=''>
+        <h1></h1>
+        {/* time.component */}
+        <nav>
+          <ul>
+            <li>ROADMAP</li>
+            <li>WORLD CURRENT</li>
+            <li>DISCORD</li>
+            <li>TWITTER</li>
+            <li>TELEGRAM</li>
+          </ul>
+        </nav>
+      </header>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
